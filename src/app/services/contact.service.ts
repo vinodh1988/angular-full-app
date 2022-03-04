219441c22b0f9384f794ba11344702ee54d42b1c
vinodh1988 <k.c.vinodh@hotmail.com>
2022-03-04 16:50:53 +0530

    service-commit

--- src/app/services/contact.service.ts
+++ src/app/services/contact.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getContacts():Observable<object>
+  {
+    return this.http.get("http://localhost:4500/contacts")
+  }
 }
